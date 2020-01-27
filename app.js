5cd2448f62f23c28b3b60f7bb915569cd8718707
James <43918220+jamxu88@users.noreply.github.com>
2020-01-27 13:47:47 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ const prefix = "=";
 client.on("message", (message) => {
 	if (message.author.id === '671411703723982889' || message.channel.id === '671421299976962068') return;
 	console.log(message.member.user.tag + ': "' + message.content + '" in channel #' + message.channel.name);
-	client.channels.get("671414066157191185").send('**' + message.member.user.tag + '**: "' + message.content + '" in channel **#' + message.channel.name + '**')
+	client.channels.get("671414066157191185").send('> **' + message.member.user.tag + '**: "' + message.content + '" in channel **#' + message.channel.name + '**')
   });
 client.login(process.env.BOT_TOKEN);
