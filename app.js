15537f6d33aec1ea2e62eceb318824a6fb0dc628
James <43918220+jamxu88@users.noreply.github.com>
2020-01-29 19:45:40 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const prefix = "=";
 client.on("message", (message) => {
 	if (message.author.id === '672235119771254785' || message.channel.id === '671421299976962068') return;
 	if (message.attachments.size > 0) {
-		console.log(message.member.user.tag + ': "' + message.content + messageattachment.url + '" in channel #' + message.channel.name);
-		client.channels.get("671414066157191185").send('> **' + message.member.user.tag + '**: "' + message.content + messageattachment.url + '" in channel **#' + message.channel.name + '**')
+		console.log(message.member.user.tag + ': "' + message.content + message.attachments.url + '" in channel #' + message.channel.name);
+		client.channels.get("671414066157191185").send('> **' + message.member.user.tag + '**: "' + message.content + message.attachments.url + '" in channel **#' + message.channel.name + '**')
 	}else {
 	console.log(message.member.user.tag + ': "' + message.content + '" in channel #' + message.channel.name);
 	client.channels.get("671414066157191185").send('> **' + message.member.user.tag + '**: "' + message.content + '" in channel **#' + message.channel.name + '**')
