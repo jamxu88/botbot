00a8e1e1a3f6146ae6f06e72fc24ed7218d8e948
James <43918220+jamxu88@users.noreply.github.com>
2020-02-11 20:02:29 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ client.on("message", (message) => {
 	if (message.author.id === '672235119771254785' || message.channel.id === '671421299976962068') return;
 	if (message.attachments.size > 0) {
 		var Attachment = (message.attachments).array();
-		console.log(message.author + ': "' + message.content + '" __with attachment:__ ' + Attachment[0].url + ' in channel #' + message.channel.name);
-		client.channels.get("671414066157191185").send('> **' + message.author + '**: "' + message.content + '" __with attachment:__ ' + Attachment[0].url + ' in channel **#' + message.channel.name + '**')
+		console.log(message.author.tag + ': "' + message.content + '" __with attachment:__ ' + Attachment[0].url + ' in channel #' + message.channel.name);
+		client.channels.get("671414066157191185").send('> **' + message.author.tag + '**: "' + message.content + '" __with attachment:__ ' + Attachment[0].url + ' in channel **#' + message.channel.name + '**')
 	}else {
-	console.log(message.author + ': "' + message.content + '" in channel #' + message.channel.name);
-	client.channels.get("671414066157191185").send('> **' + message.author + '**: "' + message.content + '" in channel **#' + message.channel.name + '**')
+	console.log(message.author.tag + ': "' + message.content + '" in channel #' + message.channel.name);
+	client.channels.get("671414066157191185").send('> **' + message.author.tag + '**: "' + message.content + '" in channel **#' + message.channel.name + '**')
 	}
   });
 client.login(process.env.BOT_TOKEN);
