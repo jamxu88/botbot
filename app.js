02807f366463f57fba3ad49e7e8df9abe98537b2
James <43918220+jamxu88@users.noreply.github.com>
2020-02-11 19:59:21 -0500

    Update app.js

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ client.on("message", (message) => {
 	if (message.attachments.size > 0) {
 		var Attachment = (message.attachments).array();
 		console.log(message.author + ': "' + message.content + '" __with attachment:__ ' + Attachment[0].url + ' in channel #' + message.channel.name);
-		client.channels.get("671414066157191185").send('> **' + message.member.user.tag + '**: "' + message.content + '" __with attachment:__ ' + Attachment[0].url + ' in channel **#' + message.channel.name + '**')
+		client.channels.get("671414066157191185").send('> **' + message.author + '**: "' + message.content + '" __with attachment:__ ' + Attachment[0].url + ' in channel **#' + message.channel.name + '**')
 	}else {
-	console.log(message.member.user.tag + ': "' + message.content + '" in channel #' + message.channel.name);
-	client.channels.get("671414066157191185").send('> **' + message.member.user.tag + '**: "' + message.content + '" in channel **#' + message.channel.name + '**')
+	console.log(message.author + ': "' + message.content + '" in channel #' + message.channel.name);
+	client.channels.get("671414066157191185").send('> **' + message.author + '**: "' + message.content + '" in channel **#' + message.channel.name + '**')
 	}
   });
 client.login(process.env.BOT_TOKEN);
