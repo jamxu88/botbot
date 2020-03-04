8f930ec33877022c8d6f67411c46ad3d08275d6e
James <43918220+jamxu88@users.noreply.github.com>
2020-03-03 20:10:34 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ client.on("ready", () => {
 });
 const prefix = "=";
 client.on("message", (message) => {
-	if (message.author.id === '672235119771254785' || message.channel.id === '671421299976962068') return;
+	if (message.author.id === '672235119771254785' || message.channel.id === '671421299976962068' || message.author.id === '684565597027041323' || message.channel.id === '684567655751221354') return;
 	if (message.attachments.size > 0) {
 		var Attachment = (message.attachments).array();
 		console.log(message.author.tag + ': "' + message.content + '" __with attachment:__ ' + Attachment[0].url + ' in channel #' + message.channel.name);
